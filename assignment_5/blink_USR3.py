c39bf7f9dde4c0c1202eb6f1c4fb9e6275543bae
OdnanG <55337864+OdnanG@users.noreply.github.com>
2019-10-08 16:59:33 -0500

    Update blink_USR3.py

--- assignment_5/blink_USR3.py
+++ assignment_5/blink_USR3.py
@@ -1,4 +1,5 @@
 """
+Copyright 2019 Odnan Galvan
 ---------------------------------------------------------------------
 Blink_USR3
 ---------------------------------------------------------------------
@@ -24,12 +25,12 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 
 for i in range(4):
-    GPIO.setup("USR%d" % i, GPIO.OUT)
+    GPIO.setup("USR3d" % i, GPIO.OUT)
 
 while True:
     for i in range(4):
-        GPIO.output("USR%d" % i, GPIO.HIGH)
-        time.sleep(1)
+        GPIO.output("USR3d" % i, GPIO.HIGH)
+        time.sleep(.200)
     for i in range(4):
-        GPIO.output("USR%d" % i, GPIO.LOW)
-        time.sleep(1)
+        GPIO.output("USR3d" % i, GPIO.LOW)
+        time.sleep(.200)
